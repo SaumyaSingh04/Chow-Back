27201c8b5f0605dc0292a75fe667fa4cc8589e75
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-12-29 09:56:16 +0530

    req

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
       "http://localhost:5173",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],//request methods
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
